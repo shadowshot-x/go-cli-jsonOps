d90253e4501f1d55ed38cc29b6ce8227f58d2fe3
ujjwal sharma <ujjwal@intugine.com>
2021-09-11 00:16:39 +0530

    hub code corrected

--- .travis/deploy_directory.sh
+++ .travis/deploy_directory.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
-docker build -t shadowshotx/jsoncliops:latest ../
+docker build -t shadowshotx/jsoncliops:latest ./
 docker push shadowshotx/jsoncliops:latest
