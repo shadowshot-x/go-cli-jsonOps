51a9eb44aa9ab99cd767cb61078bd2ece6585233
ujjwal sharma <ujjwal@intugine.com>
2021-09-11 14:10:56 +0530

    travis file changed

--- .travis/deploy_directory.sh
+++ .travis/deploy_directory.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker build -f Dockerfile -t shadowshotx/jsoncliops .
+docker build -f Dockerfile -t shadowshotx/jsoncliops:latest .
 docker push shadowshotx/jsoncliops:latest
